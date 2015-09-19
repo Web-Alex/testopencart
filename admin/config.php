1ff5e70a07c978fc2493526b8bdeef3e6323cf20
web-alex <alexxkonotop@gmail.com>
2015-09-19 08:27:33 +0300

    tochka1

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/testopencart/admin/');
+define('HTTP_CATALOG', 'http://localhost/testopencart/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/testopencart/admin/');
+define('HTTPS_CATALOG', 'http://localhost/testopencart/');
+
+// DIR
+define('DIR_APPLICATION', 'Z:/home/localhost/www/testopencart/admin/');
+define('DIR_SYSTEM', 'Z:/home/localhost/www/testopencart/system/');
+define('DIR_LANGUAGE', 'Z:/home/localhost/www/testopencart/admin/language/');
+define('DIR_TEMPLATE', 'Z:/home/localhost/www/testopencart/admin/view/template/');
+define('DIR_CONFIG', 'Z:/home/localhost/www/testopencart/system/config/');
+define('DIR_IMAGE', 'Z:/home/localhost/www/testopencart/image/');
+define('DIR_CACHE', 'Z:/home/localhost/www/testopencart/system/cache/');
+define('DIR_DOWNLOAD', 'Z:/home/localhost/www/testopencart/system/download/');
+define('DIR_UPLOAD', 'Z:/home/localhost/www/testopencart/system/upload/');
+define('DIR_LOGS', 'Z:/home/localhost/www/testopencart/system/logs/');
+define('DIR_MODIFICATION', 'Z:/home/localhost/www/testopencart/system/modification/');
+define('DIR_CATALOG', 'Z:/home/localhost/www/testopencart/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'test_opencart');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/testopencart/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/testopencart/');
+
+// DIR
+define('DIR_APPLICATION', 'Z:/home/localhost/www/testopencart/catalog/');
+define('DIR_SYSTEM', 'Z:/home/localhost/www/testopencart/system/');
+define('DIR_LANGUAGE', 'Z:/home/localhost/www/testopencart/catalog/language/');
+define('DIR_TEMPLATE', 'Z:/home/localhost/www/testopencart/catalog/view/theme/');
+define('DIR_CONFIG', 'Z:/home/localhost/www/testopencart/system/config/');
+define('DIR_IMAGE', 'Z:/home/localhost/www/testopencart/image/');
+define('DIR_CACHE', 'Z:/home/localhost/www/testopencart/system/cache/');
+define('DIR_DOWNLOAD', 'Z:/home/localhost/www/testopencart/system/download/');
+define('DIR_UPLOAD', 'Z:/home/localhost/www/testopencart/system/upload/');
+define('DIR_MODIFICATION', 'Z:/home/localhost/www/testopencart/system/modification/');
+define('DIR_LOGS', 'Z:/home/localhost/www/testopencart/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'test_opencart');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
